f48e9e3461e9378aab5d812811606aaeb305d3c0
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-14 17:36:18 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,16 @@ import App from "./App";
 test('renders App component without crashing', () => {
   render(<App />);
 });
+
+test('renders the app header', () => {
+  const stuff = render(<App />);
+  console.log(stuff);
+});
+
+// BDD: Behavior Driven Development
+// TDD: Test Driven Development
+// Both terms loosely used to describe the same pattern of writing tests first. Write a test describing what you want to happen (documentation!) and watch it fail, then work on your code until it passes.
+
+// When writing tests, be careful to avoid false positives! Sanity checks are important when writing tests for existing (already functional) code
+
+// Especially if you're the one writing the application code too (most common), think about edge cases and try to approach the question from a different point of view rather than just repeating what you did in the application code.

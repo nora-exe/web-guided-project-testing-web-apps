501fe8c75a5c750abe4a03dfe300790947441703
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-14 18:07:52 -0700

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -3,5 +3,5 @@ import { render } from "@testing-library/react";
 import AnimalForm from "./AnimalForm";
 
 test('component renders without blowing up', () => {
-    render(<AnimalForm />)
+    render(<AnimalForm />);
 })
